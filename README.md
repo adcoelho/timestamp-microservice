1d3a696f2a893c852724c923d6f17abef06e32d9
adcoelho <antoniodcoelho@gmail.com>
2016-02-17 19:24:49 +0000

    Programmed the server logic.

---
+++ README.md
@@ -0,0 +1,2 @@
+A small timestamp microservice that parses unix timestamps or natural language dates
+and returns a json object with the date in a predefined format.
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var chrono = require('chrono-node');
+var moment = require('moment');
+var app = express();
+
+
+app.get('/*', function (req, res) {
+    
+    // Get the url, eliminate the leading /, add spaces
+    var input = req.url.substring(1).replace(/%20/g, ' ');
+    var result = {
+        unix: null,
+        natural: null
+    };
+    
+    if (isNaN(input)) {
+        var date = new Date(input);
+        if (date != 'Invalid Date') {
+            result.unix = date.getTime();
+            result.natural = moment(date).format('MMMM DD, YYYY');
+        }
+    } else {
+        result.unix = +input;
+        result.natural = moment(new Date(0).setUTCSeconds(+input)).format('MMMM DD, YYYY');
+    }
+    res.end(JSON.stringify(result));
+});
+
+app.listen(8080);
